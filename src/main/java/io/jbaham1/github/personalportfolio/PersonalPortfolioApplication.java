bd272cafdc8c932e4d28e432486c960f89c38fa1
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-07 21:32:48 -0500

    portfolio structure

---
+++ src/main/java/io/jbaham1/github/personalportfolio/PersonalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package io.jbaham1.github.personalportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalPortfolioApplication.class, args);
+    }
+
+}
