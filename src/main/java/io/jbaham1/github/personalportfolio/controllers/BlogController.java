0cddc327800d6a74ebea3216488b0f87bfc4df4b
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-07 21:58:25 -0500

    feat: blog initiation index &show

--- src/main/java/io/jbaham1/github/personalportfolio/controllers/BlogController.java
+++ src/main/java/io/jbaham1/github/personalportfolio/controllers/BlogController.java
@@ -1,13 +1,25 @@
 package io.jbaham1.github.personalportfolio.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 
 @Controller
 public class BlogController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello from Spring!";
+    @GetMapping("/posts")
+    public String seeAllPosts() {
+        return "blog/index";
     }
+
+    @GetMapping("/posts/{id}")
+    public String showOnePost(@PathVariable Long id) {
+        return "/blog/show";
+    }
+
 }
+
